a08a00177f30cdad14742821fec96ed002fb959d
Mostafaemad228 <42499279+Mostafaemad228@users.noreply.github.com>
2021-11-13 16:24:01 +0200

    commit

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,3 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "300px";
-  document.getElementById("main").style.marginRight = "300px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginRight= "0";
-  document.body.style.backgroundColor = "white";
-}
-
-
-// ///////////////////////////////////// /////////////////////// ///////////////////////
 
 
 (function ($)
@@ -47,66 +33,27 @@ function closeNav() {
     
 
       
-      $(function () {
-        $("#carsour-teste").owlCarousel({
-            dots:true,
-            responsiveBaseElement:window,
-            items:1,
-            autoplay:true,
-            smartSpeed:700,
-            loop:true,
-            autoPlayHoverPause:true
-        });
-        
-      });
+
   	
-  // ////////////////////////// /////////////////////// ///////////////////////
-var typed = new Typed('.type-text', {
-  strings: ["" , "welcome to ASME-HTI." , "we aim to develop your level " , " We hope we will help you well " ],
-  typeSpeed: 150,
-  backSpeed: 50,
-  smartBackspace: true,
-  loop : true,
-  loopCount: Infinity,
-
-});
-
-
+  
   })(jQuery);
 
   
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "300px";
+      document.getElementById("main").style.marginRight = "300px";
+      document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginRight= "0";
+      document.body.style.backgroundColor = "white";
+    }
+    
   
-
-// /////////////////////// /////////////////////// ///////////////////////
   
-  // //////////////////////////////////////////////////////
-// //////////////////////////////////////////////////////
-
-
-var mySong = document.getElementById("my-song");
-var icon = document.getElementById("icon");
-icon.onclick = function () {
-  if (mySong.paused) {
-    mySong.play();
-    icon.src = "images/music/pause.png";
-  }
-  else{
-    mySong.pause();
-    icon.src = "images/music/play.png";
-  }
-}
-
-// /////////////////////////////////////////
-
-AOS.init({
-        duration: 3000,
-        once: true,
-      });
   
-
-
-// ///////////////////////////////////////////////////////////////////////////
-
   
   
 
